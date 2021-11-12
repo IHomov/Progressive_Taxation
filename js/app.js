01518f2a1aee3dd00fab00ee6db35e4243439878
IHomov <innapariy@gmail.com>
2021-11-12 15:07:14 +0200

    Update app.js
    update

--- js/app.js
+++ js/app.js
@@ -1,20 +1,21 @@
 
 const Taxa = [
-    [0, 10275, 0.1],
-    [10275, 41775, 0.12],
-    [ 41775, 89075, 0.22],
-    [ 89075, 170050, 0.24],
-    [ 170050, 215950, 0.32],
-    [215950, 539900, 0.35],
-    [539900, 100000000000, 0.37]
+    [0, 9950, 0.1],
+    [9950, 40525, 0.12],
+    [ 40525, 86375, 0.22],
+    [ 86375, 164925, 0.24],
+    [ 164925, 209425, 0.32],
+    [209425, 523600, 0.35],
+    [523600, 100000000000, 0.37]
 ]
-
-const income = 50000;
+const singleDeduction = 12550;
+let income = 50000;
+let incomeRez = income - singleDeduction;
 
 let totalTaxes = 0;
 for (let key of Taxa) {
-    if (income < key[1]) {
-        totalTaxes += (income - key[0]) * key[2];
+    if (incomeRez < key[1]) {
+        totalTaxes += (incomeRez - key[0]) * key[2];
     break;
     }
     else {
